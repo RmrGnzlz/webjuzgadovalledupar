f3dce02eaf95ff3536c57f1550180fc4abb01438
Helmer Fuentes Alvarado <elmer.fuentes@hotmail.com>
2020-12-26 14:30:53 -0500

    formulario, instalar primeng para peueba

---
+++ src/app/solitud-anonima/solitud-anonima.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-solitud-anonima',
+  templateUrl: './solitud-anonima.component.html',
+  styleUrls: ['./solitud-anonima.component.css']
+})
+export class SolitudAnonimaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
